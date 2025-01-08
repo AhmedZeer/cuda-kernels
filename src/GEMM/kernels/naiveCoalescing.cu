a4f66a804bc2c97364b50728389be68a409068ac
AhmedZeer <ahmed.zeer@std.yildiz.edu.tr>
2025-01-08 17:56:34 +0300

    coalesce-2

--- src/GEMM/kernels/naiveCoalescing.cu
+++ src/GEMM/kernels/naiveCoalescing.cu
@@ -1,13 +1,15 @@
-__global__ void naiveCoalescingGEMM(float *A, float *B, float *C, uint m, uint n, uint k, float alpha, float beta){
+__global__ void naiveCoalescingGEMM(float *A, float *B, float *C, uint m,
+                                    uint n, uint k, float alpha, float beta) {
   // C = alpha * A @ beta - beta * C
   int threadRow = blockIdx.y * blockDim.x + (threadIdx.x / blockDim.x);
   int threadCol = blockIdx.x * blockDim.x + (threadIdx.x % blockDim.x);
 
-  if( threadRow < m && threadCol < n ){
-    float tmp = 0.0f;
-    for(int i = 0 ; i < k ; i++){
-      tmp += A[threadRow * k + i] * B[i * n + threadCol];
+  if (threadRow < m && threadCol < n) {
+    float sum = 0.0f;
+    for (int i = 0; i < k; i++) {
+      sum += alpha * A[threadRow * k + i] * B[i * n + threadCol] +
+             beta * C[threadRow * n + threadCol];
     }
-    C[threadRow * n + threadCol] = alpha * tmp + beta * tmp;
-  } 
+    C[threadRow * n + threadCol] = sum;
+  }
 }
