bb9a9a8812ddc7241f888e8be49683aa327debf1
AhmedZeer <ahmed.zeer@std.yildiz.edu.tr>
2025-01-09 19:57:05 +0300

    mm no gemm

--- src/GEMM/kernels/naive.cuh
+++ src/GEMM/kernels/naive.cuh
@@ -8,9 +8,12 @@ __global__ void naiveGEMM(float *A, float *B, float *C, uint m, uint n, uint k,
   if (threadRow < m && threadCol < n) {
     float sum = 0.0f;
     for (int i = 0; i < k; i++) {
-      sum += alpha * A[threadRow * k + i] * B[i * n + threadCol] +
-             beta * C[threadRow * n + threadCol];
+      sum += A[threadRow * k + i] * B[i * n + threadCol]
     }
     C[threadRow * n + threadCol] = sum;
+    /*
+    C[threadRow * n + threadCol] =
+        alpha * sum + beta * C[threadRow * n + threadCol];
+    */
   }
 }
--- src/GEMM/kernels/naiveCoalescing.cuh
+++ src/GEMM/kernels/naiveCoalescing.cuh
@@ -11,7 +11,10 @@ __global__ void naiveCoalescingGEMM(float *A, float *B, float *C, uint m,
     for (int i = 0; i < k; i++) {
       sum += A[threadRow * k + i] * B[i * n + threadCol];
     }
+    C[threadRow * n + threadCol] = sum;
+    /*
     C[threadRow * n + threadCol] =
         alpha * sum + beta * C[threadRow * n + threadCol];
+    */
   }
 }
