7eb03ca980093610a074d157c1c0c8a5d49959eb
AhmedZeer <ahmed.zeer@std.yildiz.edu.tr>
2025-01-09 20:07:30 +0300

    fix colesce

--- src/GEMM/kernels/naiveCoalescing.cuh
+++ src/GEMM/kernels/naiveCoalescing.cuh
@@ -3,8 +3,8 @@ template <const uint BLOCKSIZE>
 __global__ void naiveCoalescingGEMM(float *A, float *B, float *C, uint m,
                                     uint n, uint k, float alpha, float beta) {
   // C = alpha * A @ beta - beta * C
-  int threadRow = blockIdx.y * BLOCKSIZE + (threadIdx.x / blockDim.x);
-  int threadCol = blockIdx.x * BLOCKSIZE + (threadIdx.x % blockDim.x);
+  int threadRow = blockIdx.y * BLOCKSIZE + (threadIdx.x / BLOCKSIZE);
+  int threadCol = blockIdx.x * BLOCKSIZE + (threadIdx.x % BLOCKSIZE);
 
   if (threadRow < m && threadCol < n) {
     float sum = 0.0f;
