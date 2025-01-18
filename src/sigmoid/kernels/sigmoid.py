7de592d1246318fb4dfdd6509690cb477a3ad9dc
AhmedZeer <ahmed.zeer@std.yildiz.edu.tr>
2025-01-18 10:26:37 +0300

    sigmoid benchmark results

--- src/sigmoid/kernels/sigmoid.py
+++ src/sigmoid/kernels/sigmoid.py
@@ -23,3 +23,54 @@ sigmoid = load(
     extra_cflags=['-std=c++17']
 )
 
+
+def run_bench(kernel, a:torch.Tensor, kernel_name:str,
+              warmup:int=10, itr:int=1000, print_res:bool=False):
+
+  for _ in range(warmup):
+    c = kernel(a)
+
+  torch.cuda.synchronize()
+
+  start = time.time()
+
+  for _ in range(itr):
+    c = kernel(a)
+
+  torch.cuda.synchronize()
+  end = time.time()
+  duration = (end - start) * 1e3
+  vals = c.flatten().detach().cpu().numpy().tolist()[-2:]
+  vals = [round(val, 6) for val in vals]
+  print(f"{vals}, duration: {duration:.2f}ms, out_{kernel_name}")
+  return c
+
+def main():
+  samples_nums = [1024, 2048, 4096]
+  features_nums = [1024, 2048, 4096]
+
+  for sample_num in samples_nums:
+    for feature_num in features_nums:
+      print("-=" * 20, f"({sample_num}, {feature_num})", "=-" * 20 )
+
+      print("\n", "-" * 20, "FP32", "-" * 20)
+      a = torch.randn([sample_num, feature_num]).cuda().float()
+
+      print()
+      run_bench(sigmoid.sigmoid_fp32_launcher, a,"f32")
+      run_bench(sigmoid.sigmoid_fp32x4_launcher, a, "f32x4")
+      run_bench(sigmoid.sigmoid_fp32x4o_launcher, a, "f32x4o")
+
+      print("\n", "-" * 20, "FP16", "-" * 20)
+      a_16f = a.half().contiguous()
+
+      print()
+      run_bench(sigmoid.sigmoid_fp16_launcher, a_16f, "f16")
+      run_bench(sigmoid.sigmoid_fp16x2o_launcher, a_16f, "f16x2o")
+      run_bench(sigmoid.sigmoid_fp16x8_launcher, a_16f, "f16x8")
+      run_bench(sigmoid.sigmoid_fp16x8_pack_launcher, a_16f, "f16x8_packed")
+      print()
+
+
+if __name__ == "__main__":
+  main()
